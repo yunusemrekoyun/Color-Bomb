096ff4c95659c29370f8149e53decb22bb5a56dc
Alpay Bilğay <alpaybil@hotmail.com>
2025-05-30 18:45:40 +0300

    .

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
